66da9943e2dd075886561f8cfb34d217c90c0920
JackPan <jheng.wei0919@gmail.com>
2023-06-27 17:41:21 +0800

    change activity name

--- app/src/main/java/com/jingyu/pay/SplashActivity.kt
+++ app/src/main/java/com/jingyu/pay/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.jingyu.pay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LauncherActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
