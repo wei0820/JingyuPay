21a9ba09355af595b561c812a795441ff73ccb2c
JackPan <jheng.wei0919@gmail.com>
2023-06-27 17:37:00 +0800

    frist commit

---
+++ app/src/main/java/com/jingyu/pay/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.jingyu.pay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jingyu.pay.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
