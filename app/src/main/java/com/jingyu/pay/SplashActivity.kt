1adf027553cc45ae1530f038f427afd1f847c1cb
JackPan <jheng.wei0919@gmail.com>
2023-06-27 18:13:06 +0800

    Fix  gradle error and add fullScreen

--- app/src/main/java/com/jingyu/pay/SplashActivity.kt
+++ app/src/main/java/com/jingyu/pay/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.jingyu.pay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,24 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+        //啟動執行序
+        Thread {
+            try {
+                Thread.sleep(2000)
+                JumpPage()
+
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start()
+
 
     }
+
+    fun JumpPage(){
+        val intent  = Intent()
+        intent.setClass(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+
 }
