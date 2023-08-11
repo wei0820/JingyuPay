fe3bde8341bba8140bc0f0be934dc7a849efc8c5
JackPan <jheng.wei0919@gmail.com>
2023-08-11 17:23:01 +0800

    commit

---
+++ app/src/main/java/com/jingyu/pay/ui/accountchange/AccountChangeViewModel.kt
@@ -0,0 +1,20 @@
+package com.jingyu.pay.ui.accountchange
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class AccountChangeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+
+    var homeViewModel = AccountChangeViewModel()
+    var  string = MutableLiveData<String>()
+
+}
