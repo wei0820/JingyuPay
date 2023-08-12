fa9779737df82d7b2e5561342450433681b7b969
JackPan <jheng.wei0919@gmail.com>
2023-08-12 16:03:21 +0800

    add PaymentQueue  and data

--- app/src/main/java/com/jingyu/pay/ui/home/HomeDateModel.kt
+++ app/src/main/java/com/jingyu/pay/ui/home/HomeDateModel.kt
@@ -1,55 +1,45 @@
 package com.jingyu.pay.ui.home
 
+import android.content.Context
 import android.util.Log
+import com.jingyu.pay.Constant
+import com.jingyu.pay.PayHelperUtils
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.io.IOException
 
+
 class HomeDateModel {
 
 
-    var BaseUrl : String = "https://api2.channel-sign.com/"
-    fun getMerchantPublicOrders(token:String,orderResponse: OrderResponse){
+    var BaseUrl : String = Constant.API_URL
+
+
+    fun setBuySetting(context: Context,orderResponse: BuyResponse){
+
         var jsonObject= JSONObject()
-        jsonObject.put("token",token)
+        jsonObject.put("token","")
         var jsonStr=jsonObject.toString()
         val contentType: MediaType = "application/json".toMediaType()
-        //调用请求
+
+
+        val urlBuilder: HttpUrl.Builder = (BaseUrl + "api/user/PaymentQueue?").toHttpUrlOrNull()!!.newBuilder()
+        urlBuilder.addQueryParameter("Paymentlimit", "5000")
+        urlBuilder.addQueryParameter("PaymentMax", "10000")
+
+
+        val url: String = urlBuilder.build().toString()
+
         val requestBody = jsonStr.toRequestBody(contentType)
         val client = OkHttpClient()
         val request = Request.Builder()
-            .url(BaseUrl + "api/android/MerchantOrders/GetMerchantPublicOrders")
-            .post(requestBody)
-            .header("content-type","application/json")
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                orderResponse.getFailure(e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                orderResponse.getResponse( response.body?.string()!!)
-            }
-        })
-
-    }
-    fun test(token:String,orderResponse: OrderResponse){
-
-        var jsonObject= JSONObject()
-        jsonObject.put("token",token)
-        var jsonStr=jsonObject.toString()
-        val contentType: MediaType = "application/json".toMediaType()
-        //调用请求
-        val requestBody = jsonStr.toRequestBody(contentType)
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url(BaseUrl + "api/user/pendingPush?")
+            .url(url)
             .get()
             .header("content-type","application/json")
-            .header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOiIxNjkwNTUxMzI4IiwiZXhwIjoxNjkwNzMxMzI4LCJpZCI6IjJiOTcyMjRhLWQ4M2MtNGQ5YS1iMjRhLWNkZWE4ZTA3ZjdmYyIsImxvZ2luSWQiOiJjZXNoaSIsImlwIjoiMTIyLjExNi4yMi40OCIsInJvbGVOYW1lIjoi5Lya5ZGYIiwiaXNzIjoiR29vZ2xlIiwiYXVkIjoid2ViIn0.Ni7vZrS2-T7TczFDqWbWdc6WxvrsFzGCDrO3WnT_4IA")
+            .header("Authorization", "Bearer " + PayHelperUtils.getUserToken(context))
             .build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -66,7 +56,41 @@ class HomeDateModel {
     }
 
 
-    interface OrderResponse{
+
+
+
+
+    fun getBuyOrederList(context: Context,orderResponse: BuyResponse){
+
+        var jsonObject= JSONObject()
+        jsonObject.put("token","")
+        var jsonStr=jsonObject.toString()
+        val contentType: MediaType = "application/json".toMediaType()
+        //调用请求
+        val requestBody = jsonStr.toRequestBody(contentType)
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(BaseUrl + "api/user/pendingPush?")
+            .get()
+            .header("content-type","application/json")
+            .header("Authorization", "Bearer " + PayHelperUtils.getUserToken(context))
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("Jack",e.toString());
+
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                orderResponse.getResponse( response.body?.string()!!)
+            }
+        })
+
+    }
+
+
+    interface BuyResponse{
         fun getResponse(s : String)
         fun getFailure(s: String)
     }
--- app/src/main/java/com/jingyu/pay/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/jingyu/pay/ui/home/HomeViewModel.kt
@@ -1,10 +1,12 @@
 package com.jingyu.pay.ui.home
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
@@ -15,25 +17,31 @@ class HomeViewModel : ViewModel() {
     val text: LiveData<String> = _text
 
     var homeViewModel = HomeDateModel()
-    var  string = MutableLiveData<String>()
+    var  startBuy = MutableLiveData<StartBuyData>()
 
 
-    fun  get() : LiveData<String>{
-        Log.d("Jack","get");
 
-        homeViewModel.test("", object : HomeDateModel.OrderResponse {
-            override fun getResponse(s: String) {
-                Log.d("Jack",s);
-                viewModelScope.launch {
-                    string.value = s;
+
+        fun getBuySetting(context: Context) : LiveData<StartBuyData> {
+            homeViewModel.setBuySetting(context, object : HomeDateModel.BuyResponse {
+                override fun getResponse(s: String) {
+                    viewModelScope.launch {
+                        if (!s.isEmpty()) {
+                            var data = Gson().fromJson(s, StartBuyData::class.java)
+                            Log.d("Jack", data.msg)
+                            Log.d("Jack", data.data.toString())
+                            startBuy.value = data
+
+                        }
+                    }
                 }
-            }
 
-            override fun getFailure(s: String) {
-            }
+                override fun getFailure(s: String) {
+                }
 
-        })
-        return  string;
-    }
+            })
+            return  startBuy
 
+
+        }
 }
