b1da0aecc0299b8e044b4b737d510099f4c3ba56
JackPan <jheng.wei0919@gmail.com>
2023-08-19 20:47:49 +0800

    crate Group Data  and APi

---
+++ app/src/main/java/com/jingyu/pay/ui/group/GroupViewModel.kt
@@ -0,0 +1,21 @@
+package com.jingyu.pay.ui.group
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+
+class GroupViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+    var groupDateModel = GroupDateModel()
+
+
+
+}
